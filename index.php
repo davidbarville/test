b23b788350ef014e8f07663958bf01a75340baa1
davidbarville <32983983+davidbarville@users.noreply.github.com>
2018-02-06 12:28:10 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- ***************************************** HEADER AND SCRIPTS ***************************************** -->
+
+<head>
+
+	<!-- borrows the Parallax template from http://materializecss.com/getting-started.html -->
+
+	<!-- IMAGES -->	
+		<!-- balloons 1: https://pixabay.com/en/adventure-balloons-colorful-1867279/ -->
+		<!-- balloons 2: https://pixabay.com/en/balloon-heart-love-romance-sky-1046658/ --> 
+		<!-- balloons 3: https://www.pexels.com/photo/blue-green-orange-yellow-and-red-balloons-on-blue-sky-106225/ -->
+		<!-- balloons 4: https://pixabay.com/en/hot-air-balloon-captive-balloon-2127203/ -->
+		
+	<!-- OLD -->
+		<!-- landscape: https://www.pexels.com/photo/grey-wooden-aisle-40030/ --> 
+		<!-- tulips: https://www.pexels.com/photo/nature-red-flowers-yellow-103573/ -->
+		<!-- sunflowers: https://www.pexels.com/photo/sky-field-flowers-blue-87056/ -->
+
+	<!-- UNUSED -->
+		<!-- tulips: http://eskipaper.com/tulip-fields-11.html#gal_post_55459_tulip-fields-11.jpg -->
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+	<title>PaperPlane Employee Benefits</title>
+
+	<!-- CSS  -->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+	<!-- fontawesome icons (personal embed code) -->
+	<script src="https://use.fontawesome.com/03e5ef3865.js"></script>
+
+</head>
+
+<!-- ***************************************** BODY START ***************************************** -->
+
+<body>
+
+<!-- ***************************************** TOP MENU ***************************************** -->
+
+	<?php include "menu.php" ?>
+
+<!-- ***************************************** MAIN PAGE ***************************************** -->
+  	
+	<!-- top banner -->
+	<div id="index-banner" class="parallax-container valign-wrapper">
+		<div class="section no-pad-bot">
+    		<div class="container">
+    			<br><br>
+        		<!--<h1 class="header center grey-text text-lighten-2">PaperPlane</h1>-->
+        			<div class="row center">
+          				<h5 class="header col s12 light">Because employee benefits don't have to be complicated</h5>
+        			</div>
+        			<div class="row center">
+          				<a href="about.html" id="download-button" class="btn-large waves-effect waves-light yellow black-text">Learn more</a>
+        			</div>
+        		<br><br>
+      		</div>
+		</div>
+    	<div class="parallax"><img src="Images/Home/background1.jpg" alt="Unsplashed background img 1"></div>
+  	</div>
+
+	<!-- icons -->
+	<div class="container">
+    	<div class="section">
+
+	      	<div class="row">
+        		<div class="col s12 m4">
+          			<div class="icon-block">
+            			<h2 class="center blue-grey-text darken-3"><i class="material-icons">flash_on</i></h2>
+           	 			<h5 class="center blue-grey-text darken-4">Engage</h5>
+		    	        <p class="light blue-grey-text darken-4">Do your employees understand the true value of the benefits package you provide, and do you stand out to them and to your industry as an employer of choice?</p>
+         	 		</div>
+        		</div>
+
+		        <div class="col s12 m4">
+    		      	<div class="icon-block">
+        	    		<h2 class="center blue-grey-text darken-3"><i class="material-icons">thumb_up</i></h2>
+            			<h5 class="center blue-grey-text darken-4">Motivate</h5>
+		        	    <p class="light blue-grey-text darken-4">Clearly and easily communicate in real-time the investment you make in every employee, and help them to appreciate what they are worth as well as what they receive.</p>
+        	  		</div>
+        		</div>
+
+	        	<div class="col s12 m4">
+    	      		<div class="icon-block">
+        	    		<h2 class="center blue-grey-text darken-3"><i class="material-icons">favorite</i></h2>
+            			<h5 class="center blue-grey-text darken-4">Inspire</h5>
+		        	    <p class="light blue-grey-text darken-4">Transform a one-way reward package with a focus on base salary into one which can help your employees to understand and manage their benefits and pension.</p>
+        	  		</div>
+        		</div>
+			</div>
+
+
+	    	</div>
+		</div>
+
+	<!-- second banner -->
+	<div class="parallax-container valign-wrapper">
+    	<div class="section no-pad-bot">
+      		<div class="container">
+        		<div class="row center">
+          			<h5 class="header col s12 light">A modern system, designed around your employees</h5>
+        		</div>
+      		</div>
+    	</div>
+    	<div class="parallax"><img src="Images/Home/background2.jpg" alt="Unsplashed background img 2"></div>
+  	</div>
+
+	<!-- next section -->
+	<div class="container">
+    	<div class="section">
+
+      		<div class="row">
+        		<div class="col s12 center">
+          			<h3><i class="mdi-content-send"></i></h3>
+          			<h4 class="blue-grey-text darken-4">Simple yet intuitive design</h4>
+          			<p class="left-align blue-grey-text darken-4">Too many employee benefit systems rely on complicated menus and hide information where . At PaperPlane, we believe that benefit communication should be simple, and our easy-to-use software puts the information front and centre. Right where your employees expect it to be.</p>
+  					<!-- carousel -->
+	  					<div class="carousel carousel-slider center" data-indicators="true">
+    						<!--<div class="carousel-fixed-item center">
+      							<a class="btn waves-effect white grey-text darken-text-2">button</a>
+    						</div>-->
+    						<div class="carousel-item grey white-text" href="#one!">
+      							<h2>First Panel</h2>
+      							<p class="grey-text">This is your first panel</p>
+    						</div>
+    						<div class="carousel-item grey white-text" href="#two!">
+      							<h2>Second Panel</h2>
+      							<p class="white-text">This is your second panel</p>
+    						</div>
+    						<div class="carousel-item grey white-text" href="#three!">
+      							<h2>Third Panel</h2>
+      							<p class="white-text">This is your third panel</p>
+    						</div>
+    						<div class="carousel-item grey white-text" href="#four!">
+      							<h2>Fourth Panel</h2>
+      							<p class="white-text">This is your fourth panel</p>
+    						</div>
+  						</div>
+        		</div>
+      		</div>
+
+    	</div>
+	</div>
+
+	<!-- third banner -->
+	<div class="parallax-container valign-wrapper">
+    	<div class="section no-pad-bot">
+      		<div class="container">
+        		<div class="row center">
+          			<h5 class="header col s12 light">something</h5>
+        		</div>
+      		</div>
+    	</div>
+    	<div class="parallax"><img src="Images/Home/background3.jpg" alt="Unsplashed background img 2"></div>
+  	</div>
+
+	<!-- icons -->
+	<div class="container">
+    	<div class="section">
+
+	      	<div class="row">
+        		<div class="col s12 m4">
+          			<div class="icon-block">
+            			<h2 class="center blue-grey-text darken-3"><i class="material-icons">access_time</i></h2>
+           	 			<h5 class="center blue-grey-text darken-4">Launch in just a week</h5>
+		    	        <p class="light blue-grey-text darken-4">Want it now? PaperPlane is extremely versatile, easily configurable, and needs no extra coding, so your new benefits portal can be up and running in just a few days. Why wait any longer?</p>
+         	 		</div>
+        		</div>
+
+		        <div class="col s12 m4">
+    		      	<div class="icon-block">
+        	    		<h2 class="center blue-grey-text darken-3"><i class="material-icons">lock</i></h2>
+            			<h5 class="center blue-grey-text darken-4">Security built-in</h5>
+		        	    <p class="light blue-grey-text darken-4">Your employees' personal data is protected by bank-level encryption. All employees will have a secure username and password to access their own data.</p>
+        	  		</div>
+        		</div>
+
+	        	<div class="col s12 m4">
+    	      		<div class="icon-block">
+        	    		<h2 class="center blue-grey-text darken-3"><i class="material-icons">dashboard</i></h2>
+            			<h5 class="center blue-grey-text darken-4">Fully modular</h5>
+		        	    <p class="light blue-grey-text darken-4">Select just the modules that you want, and relax knowing your employees will only ever be able to see the content that applies to them. Which is exactly as it should be.</p>
+        	  		</div>
+        		</div>
+			</div>
+			
+	      	<div class="row">
+        		<div class="col s12 m4">
+          			<div class="icon-block">
+            			<h2 class="center blue-grey-text darken-3"><i class="material-icons">people</i></h2>
+           	 			<h5 class="center blue-grey-text darken-4">Simple data templates</h5>
+		    	        <p class="light blue-grey-text darken-4">Set up new employees quickly and safely with data templates designed to be easy to use. Or we can work with data in your own format.</p>
+         	 		</div>
+        		</div>
+
+		        <div class="col s12 m4">
+    		      	<div class="icon-block">
+        	    		<h2 class="center blue-grey-text darken-3"><i class="material-icons">dvr</i></h2>
+            			<h5 class="center blue-grey-text darken-4">Reporting dashboard</h5>
+		        	    <p class="light blue-grey-text darken-4">View statistics and run reports whenever you feel like it from your own reporting dashboard. No need to wait for us to do it for you.</p>
+        	  		</div>
+        		</div>
+
+	        	<div class="col s12 m4">
+    	      		<div class="icon-block">
+        	    		<h2 class="center blue-grey-text darken-3"><i class="material-icons">palette</i></h2>
+            			<h5 class="center blue-grey-text darken-4">Just the way you like it</h5>
+		        	    <p class="light blue-grey-text darken-4">PaperPlane can be deployed in one of our great-looking themes, or we can easily incorporate your corporate colours and logos.</p>
+        	  		</div>
+        		</div>
+    		</div>		
+    		
+	    </div>
+	</div>
+		
+	<!-- final banner -->
+	<div class="parallax-container valign-wrapper">
+    	<div class="section no-pad-bot">
+      		<div class="container">
+        		<div class="row center">
+          			<h5 class="header col s12 light">Nothing to install. No hardware requirements.</h5>
+        		</div>
+     			<div class="row center">
+					<a class="waves-effect waves-light btn yellow black-text" onclick="$('.tap-target').tapTarget('open')">Request a demo</a>
+						<!--&nbsp;&nbsp;&nbsp;&nbsp;-->
+					<!--<a class="waves-effect waves-light btn" onclick="$('.tap-target').tapTarget('close')">Close tap target</a>-->
+        		</div>
+      		</div>
+    	</div>
+    	<div class="parallax"><img src="Images/Home/background4.jpg" alt="Unsplashed background img 3"></div>
+	</div>
+
+<!-- ***************************************** TAP TARGETS ***************************************** -->
+
+	<?php include "contact-us.html" ?>
+
+<!-- ***************************************** FOOTER ***************************************** -->
+
+	<?php include "footer.php" ?>
+
+<!-- ***************************************** SCRIPTS ***************************************** -->
+
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="js/materialize.js"></script>
+	<script src="js/init.js"></script>
+
+	<!-- carousel -->
+	<script>
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+	</script>
+
+<!-- ***************************************** END ***************************************** -->
+
+</body>
+
+</html>
